da4676607669aed47b69696c3158abbf10b3541e
Alexander <alexgo25616@gmail.com>
2015-12-01 13:41:27 +0300

    Initial Commit

---
+++ Secretary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Secretary
+//
+//  Created by Alexander on 01/12/15.
+//  Copyright © 2015 Alexander Vorontsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
